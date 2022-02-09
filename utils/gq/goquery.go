6d01c6a333a90c7f72cfbf60f00f5cfb8ab9bf51
x1a0xf <yyzw@live.com>
2022-02-09 20:14:41 +0800

    fix: 修复部分页面返回500的问题

--- utils/gq/goquery.go
+++ utils/gq/goquery.go
@@ -1,11 +1,14 @@
 package gq
 
 import (
+	"io"
 	"net/http"
 
 	"github.com/go-resty/resty/v2"
 
 	"github.com/91go/rss2/utils/log"
+
+	"github.com/gogf/gf/net/ghttp"
 
 	"github.com/sirupsen/logrus"
 
@@ -14,6 +17,37 @@ import (
 
 // FetchHTML 获取网页
 func FetchHTML(url string) *query.Document {
+	resp, err := ghttp.NewClient().Get(url)
+	if err != nil {
+		logrus.WithFields(log.Text(url, nil)).Error("http request failed")
+		return &query.Document{}
+	}
+
+	defer func(Body io.ReadCloser) {
+		if err := Body.Close(); err != nil {
+			logrus.WithFields(log.Text(url, nil)).Error("http close failed")
+		}
+	}(resp.Body)
+
+	return DocQuery(resp.Response)
+}
+
+// PostHTML 发送表单请求
+func PostHTML(url string, m map[string]interface{}) *query.Document {
+	resp, err := ghttp.NewClient().Post(url, m)
+	if err != nil {
+		return nil
+	}
+	defer func(Body io.ReadCloser) {
+		if err := Body.Close(); err != nil {
+			logrus.WithFields(log.Text(url, nil)).Error("http close failed")
+		}
+	}(resp.Response.Body)
+
+	return DocQuery(resp.Response)
+}
+
+func RestyFetchHTML(url string) *query.Document {
 	client := resty.New()
 	resp, err := client.R().Get(url)
 	if err != nil {
